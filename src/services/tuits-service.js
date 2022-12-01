6bc18a4f23b0ad49871cd4d866d062cad0d4b940
qiaojunch <qiaojunch@gmail.com>
2022-11-30 22:21:23 -0800

    a9: add backend url

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -4,7 +4,8 @@ import axios from 'axios';
 //const API_BASE = "https://tuiter-node-server-app-cs5610.herokuapp.com/api";
 
 const API_BASE = process.env.REACT_APP_API_BASE;
-const TUITS_API = 'http://localhost:4000/api/tuits' || `${API_BASE}/tuits`;
+//const TUITS_API = 'http://localhost:4000/api/tuits' || `${API_BASE}/tuits`;
+const TUITS_API = "https://tuiter-node-server-app-bjm6.onrender.com";
 
 export const createTuit = async (tuit) => {
     const response = await axios.post(TUITS_API, tuit)
